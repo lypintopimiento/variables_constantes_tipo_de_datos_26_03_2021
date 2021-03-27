9c5d7d0a1f3e3b06227c53df55b899d48aa887eb
lypintopimiento <lypintopimiento@gmail.com>
2021-03-26 20:51:03 -0500

    variables 26-03-2021

---
+++ main.js
@@ -0,0 +1,25 @@
+// Variables y constantes
+
+let nombre;
+let apellido;
+const edad = 23;
+nombre = "Ludwing";
+apellido = "Pinto";
+console.log(nombre, apellido, edad);
+
+
+// Tipos de datos
+let Nombre = `Ludwing Yesid Pinto Pimiento`; //String
+let Edad = 27; //number - intenger - int;
+let altura = 1.75; //Double - float
+let profesor = false; // Boleano
+let informacion = [Nombre, Edad, altura, profesor]; //Array
+console.log(informacion);
+// console.log(informacion[2]);
+let baso = {
+	material : "porcelan",
+	diametro :  80,
+	lleno : false
+}; // Object
+console.log(baso);
+// console.log(baso.diametro);
